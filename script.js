a16df4034d710068424312d56e5673dc48f83462
Lucas Garcia <lucasgarciaamorim@gmail.com>
2023-03-20 19:37:36 -0300

    Created Functions Add item, delete

--- script.js
+++ script.js
@@ -11,7 +11,9 @@ class Produto {
             this.Salvar(produto)
 
         }
-        console.log(this.arrayProdutos)
+        this.Listar()
+        this.Cancelar()
+
 
     }
     lerDados() {
@@ -29,19 +31,61 @@ class Produto {
 
         if (produto.nomeProduto == '') {
             msg += 'Por favor, insira corretamente o nome do Produto! '
-        }
+        };
         if (produto.precoProduto == '') {
             msg += 'Por favor, insira corretamente o preço do Produto! '
-        }
+        };
         if (msg != '') {
-            alert(msg)
-            return false
+            alert(msg);
+            return false;
         }
-        return true
+        return true;
     }
     Salvar(produto) {
         this.arrayProdutos.push(produto)
         this.id++;
+    }
+    Listar() {
+        let tbody = document.querySelector('#tbody');
+        tbody.innerHTML = ''
+
+        for (let i = 0; i < this.arrayProdutos.length; i++) {
+            let tr = tbody.insertRow();
+
+            let td_id = tr.insertCell();
+            let td_nome = tr.insertCell();
+            let td_preco = tr.insertCell();
+            let td_del = tr.insertCell();
+
+            td_id.innerText = this.arrayProdutos[i].id;
+            td_nome.innerText = this.arrayProdutos[i].nomeProduto;
+            td_preco.innerText = this.arrayProdutos[i].precoProduto;
+            let imagem = document.createElement('img');
+            imagem.src = '/assets/img/lixeira.jpg'
+            imagem.setAttribute('onclick', "produto.Deletar(" + this.arrayProdutos[i].id + ")")
+            td_del.appendChild(imagem);
+
+
+
+        }
+    }
+
+    Cancelar() {
+        document.querySelector('#nomeProduto').value = null;
+        document.querySelector('#precoProduto').value = null;
+
+    }
+    Deletar(id) {
+        let tbody = document.querySelector('#tbody');
+        for (let i = 0; i < this.arrayProdutos.length; i++) {
+            if(this.arrayProdutos[i].id == id){
+                this.arrayProdutos.splice(i , 1)
+                tbody.deleteRow(i);
+            }
+            
+        }
+        alert('O item foi apagado com sucesso')
+
     }
 }
 
